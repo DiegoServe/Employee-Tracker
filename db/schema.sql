4e4de790ada287a83ccb8979939dfa992ee787da
DiegoServe <110145883+DiegoServe@users.noreply.github.com>
2022-10-26 19:55:55 -0400

    connected and Welcome page

--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,7 @@ USE employee_db;
 
 -- Table for department
 CREATE TABLE department (
-    id INTERGER PRIMARY KEY AUTO_INCREMENT,
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL
 );
 
@@ -24,9 +24,8 @@ CREATE TABLE employee (
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER, 
     INDEX role_ind (role_id),
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
     manager_id INTEGER,
     INDEX manager_ind (manager_id),
     CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
-
--- server.js
+++ server.js
@@ -1,4 +1,28 @@
 // Import packages
-const mysql = require('mysql2')
-const inquirer = require('inquirer'); 
-const cTable = require('console.table');
+const mysql = require("mysql2");
+const inquirer = require("inquirer");
+const cTable = require("console.table");
+
+require("dotenv").config();
+
+// connection to database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: process.env.MYSQL_PASSWORD,
+  database: 'employee_db'
+});
+
+connection.connect(err => {
+  if (err) throw err;
+  console.log('connected as id ' + connection.threadId);
+  afterConnection();
+});
+
+// Prompts and Welcome Page`
+afterConnection = () => {
+  console.log("===================================")
+  console.log("         EMPLOYEE TRACKER          ")
+  console.log("===================================")
+  promptUser();
+};
